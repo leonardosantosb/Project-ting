b39ed83412e95ae1efb46560cf616a07dbd234ed
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-07-14 16:57:51 -0300

    req 3 - implementando a funçao process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_names = [data['nome_do_arquivo'] for data in instance.queue]
+    if path_file in file_names:
+        print(f'O arquivo {path_file} já foi processado anteriormente.')
+        return
+
+    lines = txt_importer(path_file)
+    if lines:
+        file_data = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len(lines),
+            'linhas_do_arquivo': lines
+        }
+        instance.enqueue(file_data)
+        print(file_data)
+    else:
+        print(f'Não foi possível processar o arquivo {path_file}.')
 
 
 def remove(instance):
